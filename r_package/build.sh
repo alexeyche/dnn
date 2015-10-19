c5f6865ccff1c4e05a9b490170b9f1fbecb1d30c
alexeyche <alexey.chernushev@gmail.com>
2015-10-19 22:25:35 +0300

    cns

--- r_package/build.sh
+++ r_package/build.sh
@@ -6,8 +6,8 @@ set -x
 pushd $packagename
 Rscript -e 'Rcpp::compileAttributes()'
 popd
-DNN_INCLUDE=${DNN_INCLUDE:-~/dnn/include/dnn_project}
-DNN_LIB=${DNN_LIB:-~/dnn/lib}
+DNN_INCLUDE=${DNN_INCLUDE:-$DNN_HOME/include/dnn_project}
+DNN_LIB=${DNN_LIB:-$DNN_HOME/lib}
 R CMD build $packagename
 R CMD INSTALL --build ${packagename}_${ver}.tar.gz --configure-args="--with-dnn-include=$DNN_INCLUDE --with-dnn-lib=$DNN_LIB"
 popd
