bd57232e9459f31a0efbd2765888225cf6f4e0d0
Alexey <alexey.kozov@gmail.com>
2015-11-28 20:08:56 +0300

    hedonistic synapse - add refractory - rename "tau_trace" to "tau_eligibility" - add exceptions

--- dnn_project/dnn/synapses/hedonistic_synapse.h
+++ dnn_project/dnn/synapses/hedonistic_synapse.h
@@ -10,40 +10,55 @@ namespace dnn {
 
 /*@GENERATE_PROTO@*/
 struct HedonisticSynapseC : public Serializable<Protos::HedonisticSynapseC>  {
-    HedonisticSynapseC() : psp_decay(10.0), amp(1.0), delta_catalyst(0.25), tau_catalyst(500), tau_trace(750), learning_rate(0.25) {}
+    HedonisticSynapseC() : psp_decay(10.0), amp(1.0), tau_ref(350), delta_catalyst(0.25), tau_catalyst(500), tau_eligibility(750), learning_rate(0.25) {}
 
     void serial_process() {
         begin() << "psp_decay: "        << psp_decay       << ", " \
                 << "amp: "              << amp             << ", " \
+                << "tau_ref: "          << tau_ref         << ", " \
                 << "delta_catalyst: "   << delta_catalyst  << ", " \
                 << "tau_catalyst: "     << tau_catalyst    << ", " \
-                << "tau_trace: "        << tau_trace       << ", " \
+                << "tau_eligibility: "  << tau_eligibility << ", " \
                 << "learning_rate: "    << learning_rate   << Self::end;
+
+    double zero = numeric_limits<double>::epsilon();
+    if(fabs(tau_ref) <= zero) {
+        throw dnnException() << "Time constant tau_ref is too small, division by zero\n";
+            }
+    if(fabs(tau_catalyst) <= zero) {
+        throw dnnException() << "Time constant tau_catalyst is too small, division by zero\n";
+            }
+    if(fabs(tau_eligibility) <= zero) {
+        throw dnnException() << "Time constant tau_eligibility is too small, division by zero\n";
+            }
     }
 
     double psp_decay;
     double amp;
+    double tau_ref;
     double delta_catalyst;
     double tau_catalyst;
-    double tau_trace;
+    double tau_eligibility;
     double learning_rate;
 };
 
 /*@GENERATE_PROTO@*/
 struct HedonisticSynapseState : public Serializable<Protos::HedonisticSynapseState>  {
-    HedonisticSynapseState() : probability(0.5), catalyst(0.0), prob_weight(0), spike_trace(0) {}
+    HedonisticSynapseState() : refractory (0), probability(0.5), catalyst(0.0), prob_weight(0), eligibility_trace(0) {}
 
     void serial_process() {
-        begin() << "probability: "  << probability  << ", " \
+        begin() << "refractory: "   << refractory   << ", " \
+                << "probability: "  << probability  << ", " \
                 << "catalyst: "     << catalyst     << ", " \
                 << "prob_weight: "  << prob_weight  << ", " \
-                << "spike_trace: "  << spike_trace  << Self::end;
+                << "eligibility_trace: "  << eligibility_trace  << Self::end;
     }
     
+    double refractory;
     double probability;
-    double catalyst;    // c
-    double prob_weight; // q
-    double spike_trace; // e
+    double catalyst;            // c
+    double prob_weight;         // q
+    double eligibility_trace;   // e
 };
 
 class HedonisticSynapse : public Synapse<HedonisticSynapseC, HedonisticSynapseState> {
@@ -55,37 +70,39 @@ public:
     void reset() {
         mutAmplitude() = c.amp;
         mutPotential() = 0;
+        s.refractory = 0;
     }
 
     void propagateSpike() {
-
-        s.probability = 1.0 / (1.0 + exp(-s.prob_weight - s.catalyst));
-        if (s.probability > getUnif()) {
-            // success
-            mutPotential() += c.amp;
-            s.spike_trace += 1 - s.probability;
-        }
-        else {
-            s.spike_trace += -s.probability;
+        if (s.refractory < 0.1) {
+            s.probability = 1.0 / (1.0 + exp(-s.prob_weight - s.catalyst));
+            if (s.probability > getUnif()) {
+                // release
+                mutPotential() += c.amp;
+                s.eligibility_trace += 1 - s.probability;
+                s.refractory = c.tau_ref;
+            }
+            else {
+                // failure
+                s.eligibility_trace += -s.probability;
+            }
         }
         s.catalyst += c.delta_catalyst;
     }
 
     void calculateDynamics(const Time &t) {
         stat.add("x", potential());
-        stat.add("spike_trace", s.spike_trace);
+        stat.add("eligibility_trace", s.eligibility_trace);
         stat.add("prob_weight", s.prob_weight);
 
         mutPotential() += - t.dt * potential()/c.psp_decay;
 
-        s.spike_trace += -t.dt * s.spike_trace/c.tau_trace;
-        s.catalyst += -t.dt * s.catalyst/c.tau_catalyst;
-        s.prob_weight += c.learning_rate * s.spike_trace * GlobalCtx::inst().getReward();
+        s.eligibility_trace += - t.dt * s.eligibility_trace/c.tau_eligibility;
+        s.catalyst += - t.dt * s.catalyst/c.tau_catalyst;
+        s.refractory += - t.dt * s.refractory/c.tau_ref;
+        s.prob_weight += c.learning_rate * s.eligibility_trace * GlobalCtx::inst().getReward();
 
     }
 };
-
-
-
 
 }
